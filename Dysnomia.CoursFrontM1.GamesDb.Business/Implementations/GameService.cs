3f43148fbfcd8f65cae33b4cd4ccf1a567b83097
Elanis <elanis@hotmail.com>
2024-04-15 18:54:38 +0200

    Add some wild cache to prevent tanking IGDB API

--- Dysnomia.CoursFrontM1.GamesDb.Business/Implementations/GameService.cs
+++ Dysnomia.CoursFrontM1.GamesDb.Business/Implementations/GameService.cs
@@ -13,6 +13,7 @@ namespace Dysnomia.CoursFrontM1.GamesDb.Business.Implementations {
             this.IGDBClient = new IGDBClient(options?.Value?.IGDBClientId, options?.Value?.IGDBClientSecret);
         }
 
+        private static Dictionary<(uint, uint), (DateTime, Game[])> getGamesCache = [];
         public async Task<Game[]> GetGames(uint pageSize, uint page) {
             if (pageSize < 5 || pageSize > 500) {
                 throw new InvalidDataException("Page size must be between 5 and 500");
@@ -22,6 +23,12 @@ namespace Dysnomia.CoursFrontM1.GamesDb.Business.Implementations {
                 throw new InvalidDataException("Page number must greater or equal to one");
             }
 
+            if (getGamesCache.TryGetValue((pageSize, page), out var gamesCache)) {
+                if (gamesCache.Item1 - DateTime.UtcNow < TimeSpan.FromHours(1)) {
+                    return gamesCache.Item2;
+                }
+            }
+
             var request = $"fields *; sort rating desc; where rating != null; limit {pageSize};";
             if (page > 1) {
                 request += $"offset {pageSize * (page - 1)};";
@@ -29,9 +36,12 @@ namespace Dysnomia.CoursFrontM1.GamesDb.Business.Implementations {
 
             var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
 
+            getGamesCache[(pageSize, page)] = (DateTime.UtcNow, games);
+
             return games;
         }
 
+        private static Dictionary<(string, uint, uint), (DateTime, Game[])> searchGamesCache = [];
         public async Task<Game[]> SearchGames(string term, uint pageSize, uint page) {
             if (pageSize < 5 || pageSize > 500) {
                 throw new InvalidDataException("Page size must be between 5 and 500");
@@ -41,6 +51,12 @@ namespace Dysnomia.CoursFrontM1.GamesDb.Business.Implementations {
                 throw new InvalidDataException("Page number must greater or equal to one");
             }
 
+            if (searchGamesCache.TryGetValue((term, pageSize, page), out var gamesCache)) {
+                if (gamesCache.Item1 - DateTime.UtcNow < TimeSpan.FromHours(1)) {
+                    return gamesCache.Item2;
+                }
+            }
+
             var request = $"fields *; search \"{term}\"; limit {pageSize};";
             if (page > 1) {
                 request += $"offset {pageSize * (page - 1)};";
@@ -48,15 +64,29 @@ namespace Dysnomia.CoursFrontM1.GamesDb.Business.Implementations {
 
             var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
 
+            searchGamesCache[(term, pageSize, page)] = (DateTime.UtcNow, games);
+
             return games;
         }
 
+        private static Dictionary<ulong, (DateTime, Game)> gamesCache = [];
         public async Task<Game?> GetGameById(ulong id) {
+            if (gamesCache.TryGetValue(id, out var gameCache)) {
+                if (gameCache.Item1 - DateTime.UtcNow < TimeSpan.FromHours(1)) {
+                    return gameCache.Item2;
+                }
+            }
+
             var request = $"fields *; sort rating desc; where id = {id};";
 
             var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
 
-            return games.FirstOrDefault();
+            var game = games.FirstOrDefault();
+            if (game is not null) {
+                gamesCache[id] = (DateTime.UtcNow, game);
+            }
+
+            return game;
         }
     }
 }
