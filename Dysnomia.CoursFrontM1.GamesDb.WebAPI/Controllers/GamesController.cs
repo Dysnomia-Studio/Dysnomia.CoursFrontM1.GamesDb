5901c527d193453d38ea20ebbd6d403de345bd54
Elanis <elanis@hotmail.com>
2024-04-18 10:11:37 +0200

    Improve controllers documentation

--- Dysnomia.CoursFrontM1.GamesDb.WebAPI/Controllers/GamesController.cs
+++ Dysnomia.CoursFrontM1.GamesDb.WebAPI/Controllers/GamesController.cs
@@ -18,6 +18,8 @@ namespace Dysnomia.CoursFrontM1.GamesDb.WebAPI.Controllers {
 
 		[AllowAnonymous]
 		[HttpGet("top")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		public async Task<ActionResult<Game[]>> GetGames(uint pageSize = 10, uint page = 1) {
 			try {
 				return Ok(await gameService.GetGames(pageSize, page));
@@ -27,6 +29,9 @@ namespace Dysnomia.CoursFrontM1.GamesDb.WebAPI.Controllers {
 		}
 
 		[HttpGet("search")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
 		public async Task<ActionResult<Game[]>> SearchGames(string term, uint pageSize = 10, uint page = 1) {
 			try {
 				return Ok(await gameService.SearchGames(term, pageSize, page));
@@ -36,6 +41,8 @@ namespace Dysnomia.CoursFrontM1.GamesDb.WebAPI.Controllers {
 		}
 
 		[HttpGet("{id}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
 		public async Task<ActionResult<Game>> GetGameById(ulong id) {
 			return Ok(await gameService.GetGameById(id));
 		}
--- Dysnomia.CoursFrontM1.GamesDb.WebAPI/Controllers/UsersController.cs
+++ Dysnomia.CoursFrontM1.GamesDb.WebAPI/Controllers/UsersController.cs
@@ -7,83 +7,94 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dysnomia.CoursFrontM1.GamesDb.WebAPI.Controllers {
-    [Authorize(Policy = AuthPolicies.JWT_POLICY)]
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase {
-        private readonly IUserService usersService;
-        public UsersController(IUserService usersService) {
-            this.usersService = usersService;
-        }
+	[Authorize(Policy = AuthPolicies.JWT_POLICY)]
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UsersController : ControllerBase {
+		private readonly IUserService usersService;
+		public UsersController(IUserService usersService) {
+			this.usersService = usersService;
+		}
 
-        [AllowAnonymous]
-        [HttpPost("auth")]
-        public async Task<ActionResult<string>> AuthenticateUser(UserAuthenticationRequest authenticationRequest) {
-            var token = await usersService.AuthenticateUser(authenticationRequest.Username, authenticationRequest.Password);
+		[AllowAnonymous]
+		[HttpPost("auth")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		public async Task<ActionResult<string>> AuthenticateUser(UserAuthenticationRequest authenticationRequest) {
+			var token = await usersService.AuthenticateUser(authenticationRequest.Username, authenticationRequest.Password);
 
-            if (token == null) {
-                return Unauthorized();
-            }
+			if (token == null) {
+				return Unauthorized();
+			}
 
-            return Ok(token);
-        }
+			return Ok(token);
+		}
 
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public async Task<ActionResult<string>> RegisterUser(UserRegistrationRequest registrationRequest) {
-            try {
-                var token = await usersService.Register(registrationRequest);
+		[AllowAnonymous]
+		[HttpPost("register")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public async Task<ActionResult<string>> RegisterUser(UserRegistrationRequest registrationRequest) {
+			try {
+				var token = await usersService.Register(registrationRequest);
 
-                return Ok(token);
-            } catch (InvalidDataException ex) {
-                return BadRequest(ex.Message);
-            }
-        }
+				return Ok(token);
+			} catch (InvalidDataException ex) {
+				return BadRequest(ex.Message);
+			}
+		}
 
-        [HttpGet("renewToken")]
-        public ActionResult<string> RenewToken() {
-            var name = HttpContext?.User?.Identity?.Name;
-            if (name == null) {
-                return NoContent();
-            }
-            var token = usersService.RenewToken(name);
+		[HttpGet("renewToken")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		public ActionResult<string> RenewToken() {
+			var name = HttpContext?.User?.Identity?.Name;
+			if (name == null) {
+				return NoContent();
+			}
+			var token = usersService.RenewToken(name);
 
-            if (token == default) {
-                return NoContent();
-            }
+			if (token == default) {
+				return NoContent();
+			}
 
-            return Ok(token);
-        }
+			return Ok(token);
+		}
 
-        [HttpGet("me")]
-        public async Task<ActionResult<UserDto>> GetUserFavoriteGames() {
-            var user = await usersService.GetByUsername(HttpContext?.User?.Identity?.Name);
+		[HttpGet("me")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		public async Task<ActionResult<UserDto>> GetUserFavoriteGames() {
+			var user = await usersService.GetByUsername(HttpContext?.User?.Identity?.Name);
 
-            if (user == default) {
-                return NoContent();
-            }
+			if (user == default) {
+				return NoContent();
+			}
 
-            return Ok(user);
-        }
+			return Ok(user);
+		}
 
-        [HttpPost("favorites/add/{gameId}")]
-        public async Task<ActionResult> AddGameToFavorites(ulong gameId) {
-            try {
-                await usersService.AddGameToFavorites(HttpContext?.User?.Identity?.Name, gameId);
-                return NoContent();
-            } catch (InvalidDataException) {
-                return Conflict();
-            }
-        }
+		[HttpPost("favorites/add/{gameId}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		[ProducesResponseType(StatusCodes.Status409Conflict)]
+		public async Task<ActionResult> AddGameToFavorites(ulong gameId) {
+			try {
+				await usersService.AddGameToFavorites(HttpContext?.User?.Identity?.Name, gameId);
+				return NoContent();
+			} catch (InvalidDataException) {
+				return Conflict();
+			}
+		}
 
-        [HttpDelete("favorites/remove/{gameId}")]
-        public async Task<ActionResult> RemoveGameFromFavorites(ulong gameId) {
-            try {
-                await usersService.RemoveGameFromFavorites(HttpContext?.User?.Identity?.Name, gameId);
-                return NoContent();
-            } catch (InvalidDataException) {
-                return Conflict();
-            }
-        }
-    }
+		[HttpDelete("favorites/remove/{gameId}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+		public async Task<ActionResult> RemoveGameFromFavorites(ulong gameId) {
+			await usersService.RemoveGameFromFavorites(HttpContext?.User?.Identity?.Name, gameId);
+			return NoContent();
+		}
+	}
 }
