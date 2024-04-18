9af09b3b9e6b20d511b372a32ed0ea6413b3410d
Elanis <elanis@hotmail.com>
2024-04-18 09:48:33 +0200

    Passage de api/games/top en anonyme + augmentation du cache

--- Dysnomia.CoursFrontM1.GamesDb.Business/Implementations/GameService.cs
+++ Dysnomia.CoursFrontM1.GamesDb.Business/Implementations/GameService.cs
@@ -7,86 +7,86 @@ using IGDB.Models;
 using Microsoft.Extensions.Options;
 
 namespace Dysnomia.CoursFrontM1.GamesDb.Business.Implementations {
-    public class GameService : IGameService {
-        private readonly IGDBClient IGDBClient;
-        public GameService(IOptions<AppSettings> options) {
-            this.IGDBClient = new IGDBClient(options?.Value?.IGDBClientId, options?.Value?.IGDBClientSecret);
-        }
+	public class GameService : IGameService {
+		private readonly IGDBClient IGDBClient;
+		public GameService(IOptions<AppSettings> options) {
+			this.IGDBClient = new IGDBClient(options?.Value?.IGDBClientId, options?.Value?.IGDBClientSecret);
+		}
 
-        private static readonly Dictionary<(uint, uint), (DateTime, Game[])> getGamesCache = [];
-        public async Task<Game[]> GetGames(uint pageSize, uint page) {
-            if (pageSize < 5 || pageSize > 500) {
-                throw new InvalidDataException("Page size must be between 5 and 500");
-            }
+		private static readonly Dictionary<(uint, uint), (DateTime, Game[])> getGamesCache = [];
+		public async Task<Game[]> GetGames(uint pageSize, uint page) {
+			if (pageSize < 5 || pageSize > 500) {
+				throw new InvalidDataException("Page size must be between 5 and 500");
+			}
 
-            if (page < 1) {
-                throw new InvalidDataException("Page number must greater or equal to one");
-            }
+			if (page < 1) {
+				throw new InvalidDataException("Page number must greater or equal to one");
+			}
 
-            if (getGamesCache.TryGetValue((pageSize, page), out var gameCache)) {
-                if (gameCache.Item1 - DateTime.UtcNow < TimeSpan.FromHours(1)) {
-                    return gameCache.Item2;
-                }
-            }
+			if (getGamesCache.TryGetValue((pageSize, page), out var gameCache)) {
+				if (gameCache.Item1 - DateTime.UtcNow < TimeSpan.FromDays(1)) {
+					return gameCache.Item2;
+				}
+			}
 
-            var request = $"fields *; sort rating desc; where rating != null; limit {pageSize};";
-            if (page > 1) {
-                request += $"offset {pageSize * (page - 1)};";
-            }
+			var request = $"fields *; sort rating desc; where rating != null; limit {pageSize};";
+			if (page > 1) {
+				request += $"offset {pageSize * (page - 1)};";
+			}
 
-            var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
+			var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
 
-            getGamesCache[(pageSize, page)] = (DateTime.UtcNow, games);
+			getGamesCache[(pageSize, page)] = (DateTime.UtcNow, games);
 
-            return games;
-        }
+			return games;
+		}
 
-        private static readonly Dictionary<(string, uint, uint), (DateTime, Game[])> searchGamesCache = [];
-        public async Task<Game[]> SearchGames(string term, uint pageSize, uint page) {
-            if (pageSize < 5 || pageSize > 500) {
-                throw new InvalidDataException("Page size must be between 5 and 500");
-            }
+		private static readonly Dictionary<(string, uint, uint), (DateTime, Game[])> searchGamesCache = [];
+		public async Task<Game[]> SearchGames(string term, uint pageSize, uint page) {
+			if (pageSize < 5 || pageSize > 500) {
+				throw new InvalidDataException("Page size must be between 5 and 500");
+			}
 
-            if (page < 1) {
-                throw new InvalidDataException("Page number must greater or equal to one");
-            }
+			if (page < 1) {
+				throw new InvalidDataException("Page number must greater or equal to one");
+			}
 
-            if (searchGamesCache.TryGetValue((term, pageSize, page), out var gameCache)) {
-                if (gameCache.Item1 - DateTime.UtcNow < TimeSpan.FromHours(1)) {
-                    return gameCache.Item2;
-                }
-            }
+			if (searchGamesCache.TryGetValue((term, pageSize, page), out var gameCache)) {
+				if (gameCache.Item1 - DateTime.UtcNow < TimeSpan.FromHours(1)) {
+					return gameCache.Item2;
+				}
+			}
 
-            var request = $"fields *; search \"{term}\"; limit {pageSize};";
-            if (page > 1) {
-                request += $"offset {pageSize * (page - 1)};";
-            }
+			var request = $"fields *; search \"{term}\"; limit {pageSize};";
+			if (page > 1) {
+				request += $"offset {pageSize * (page - 1)};";
+			}
 
-            var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
+			var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
 
-            searchGamesCache[(term, pageSize, page)] = (DateTime.UtcNow, games);
+			searchGamesCache[(term, pageSize, page)] = (DateTime.UtcNow, games);
 
-            return games;
-        }
+			return games;
+		}
 
-        private static readonly Dictionary<ulong, (DateTime, Game)> gamesCache = [];
-        public async Task<Game?> GetGameById(ulong id) {
-            if (gamesCache.TryGetValue(id, out var gameCache)) {
-                if (gameCache.Item1 - DateTime.UtcNow < TimeSpan.FromHours(1)) {
-                    return gameCache.Item2;
-                }
-            }
+		private static readonly Dictionary<ulong, (DateTime, Game)> gamesCache = [];
+		public async Task<Game?> GetGameById(ulong id) {
+			if (gamesCache.TryGetValue(id, out var gameCache)) {
+				if (gameCache.Item1 - DateTime.UtcNow < TimeSpan.FromHours(1)) {
+					return gameCache.Item2;
+				}
+			}
 
-            var request = $"fields *; sort rating desc; where id = {id};";
+			var request = $"fields *; sort rating desc; where id = {id};";
 
-            var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
+			var games = await IGDBClient.QueryAsync<Game>(IGDBClient.Endpoints.Games, request);
 
-            var game = games.FirstOrDefault();
-            if (game is not null) {
-                gamesCache[id] = (DateTime.UtcNow, game);
-            }
+			var game = games.FirstOrDefault();
+			if (game is not null) {
+				gamesCache[id] = (DateTime.UtcNow, game);
+			}
 
-            return game;
-        }
-    }
+			return game;
+		}
+	}
 }
--- Dysnomia.CoursFrontM1.GamesDb.WebAPI/Controllers/GamesController.cs
+++ Dysnomia.CoursFrontM1.GamesDb.WebAPI/Controllers/GamesController.cs
@@ -7,36 +7,37 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dysnomia.CoursFrontM1.GamesDb.WebAPI.Controllers {
-    [Authorize(Policy = AuthPolicies.JWT_POLICY)]
-    [ApiController]
-    [Route("api/[controller]")]
-    public class GamesController : ControllerBase {
-        private readonly IGameService gameService;
-        public GamesController(IGameService gameService) {
-            this.gameService = gameService;
-        }
+	[Authorize(Policy = AuthPolicies.JWT_POLICY)]
+	[ApiController]
+	[Route("api/[controller]")]
+	public class GamesController : ControllerBase {
+		private readonly IGameService gameService;
+		public GamesController(IGameService gameService) {
+			this.gameService = gameService;
+		}
 
-        [HttpGet("top")]
-        public async Task<ActionResult<Game[]>> GetGames(uint pageSize = 10, uint page = 1) {
-            try {
-                return Ok(await gameService.GetGames(pageSize, page));
-            } catch (InvalidDataException ex) {
-                return BadRequest(ex.Message);
-            }
-        }
+		[AllowAnonymous]
+		[HttpGet("top")]
+		public async Task<ActionResult<Game[]>> GetGames(uint pageSize = 10, uint page = 1) {
+			try {
+				return Ok(await gameService.GetGames(pageSize, page));
+			} catch (InvalidDataException ex) {
+				return BadRequest(ex.Message);
+			}
+		}
 
-        [HttpGet("search")]
-        public async Task<ActionResult<Game[]>> SearchGames(string term, uint pageSize = 10, uint page = 1) {
-            try {
-                return Ok(await gameService.SearchGames(term, pageSize, page));
-            } catch (InvalidDataException ex) {
-                return BadRequest(ex.Message);
-            }
-        }
+		[HttpGet("search")]
+		public async Task<ActionResult<Game[]>> SearchGames(string term, uint pageSize = 10, uint page = 1) {
+			try {
+				return Ok(await gameService.SearchGames(term, pageSize, page));
+			} catch (InvalidDataException ex) {
+				return BadRequest(ex.Message);
+			}
+		}
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Game>> GetGameById(ulong id) {
-            return Ok(await gameService.GetGameById(id));
-        }
-    }
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Game>> GetGameById(ulong id) {
+			return Ok(await gameService.GetGameById(id));
+		}
+	}
 }
